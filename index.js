54e9eefaae2e118e7e6c719d831d75440241020c
Wissam Charafeddine <wissam.charafeddine@gmail.com>
2016-03-29 20:42:46 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var resp = prompt('PLEASE ENTER YOUR NAME:');
+
+// function makeMoreExciting(string) {
+// 	return string + '!!!';
+// }
+// function yellIt(string) {
+//   string = string.toUpperCase();
+//   string = makeMoreExciting(string);
+//   console.log(string);
+// }
+//
+// yellIt("Hello!");
+// console.log(resp);
+
+var element = document.getElementById('note');
+element.innerHTML = "Thank You";
